945146d13e0050e1bcb2e55eaaf42f2206915f9c
Frikk Jarl <Frikk@jarl.email>
2025-02-05 12:57:58 +0100

    Update grid-row.query.ts

--- components/ui/grid/grid-row/grid-row.query.ts
+++ components/ui/grid/grid-row/grid-row.query.ts
@@ -1,8 +1,9 @@
 import { groq } from "next-sanity";
 // Update paths for the queries based on your reorg
 import { gridCardQuery } from "@/components/ui/grid/grid-card/grid-card.query";
-import { pricingCardQuery } from "@/components/ui/pricing-card/grid-pricing-card.query";
-import { gridPostQuery } from "@/components/ui/grid-post/grid-post.query";
+import { pricingCardQuery } from "../pricing-card/pricing-card.query";
+import { gridPostQuery } from "../grid-post/grid-post.query";
+
 
 export const gridRowQuery = groq`
   _type == "grid-row" => {
