0af495ff2bfc6764dfc784c910ec12a33894c639
Frikk Jarl <Frikk@jarl.email>
2025-03-24 13:18:19 +0100

    Update heading-and-paragraph-centered.block-component.tsx

--- features/page-builder-blocks/blocks/heading-and-paragraph-centered-block/heading-and-paragraph-centered.block-component.tsx
+++ features/page-builder-blocks/blocks/heading-and-paragraph-centered-block/heading-and-paragraph-centered.block-component.tsx
@@ -12,7 +12,7 @@ Partial<HeadingAndParagraphCenteredProps>) {
   return (
     <>
       
-      <Section className="py-20">
+      <Section className="py-20  pt-12">
         <Container className="text-center items-center gap-y-6 max-w-screen-md ">
           <h2 className=" font-semibold max-w-[550px] md:text-5xl text-balance text-4xl tracking-wider font-title text-center">{props.heading}</h2>
           <p className="text-lg text-center">{props.paragraph}</p>
